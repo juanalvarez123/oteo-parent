86440d705ba7f83b5d4ee22b5edcd3635da87144
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2017-09-12 22:55:25 -0500

    First version

---
+++ oteo-rest-api/src/main/java/com/oteo/controller/Test1.java
@@ -0,0 +1,25 @@
+package com.oteo.controller;
+
+/**
+ *
+ *
+ * @author Juan Sebastian Alvarez Eraso
+ * @project oteo-rest-api
+ * @date 11/09/2017
+ *
+ */
+public class Test1 {
+
+	/**
+	 *
+	 *
+	 * @author Juan Sebastian Alvarez Eraso
+	 * @date 11/09/2017
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
