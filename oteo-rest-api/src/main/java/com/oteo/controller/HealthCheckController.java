288181de0cee28475161615f8dd4b3f830473ae4
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2017-09-12 23:32:34 -0500

    First REST service created

---
+++ oteo-rest-api/src/main/java/com/oteo/controller/HealthCheckController.java
@@ -0,0 +1,18 @@
+package com.oteo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class HealthCheckController {
+
+	@RequestMapping(path = "/healthcheck", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ApiGenericResponse getHealthCheck() {
+
+		return new ApiGenericResponse("First message");
+	}
+
+}
